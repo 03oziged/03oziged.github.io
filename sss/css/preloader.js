4db51d62edb0170f0cf274d5f41f4e21acb244e6
bgg <zheka.derevyanko@gmail.com>
2018-10-11 16:11:33 +0300

    deleted

--- sss/css/preloader.js
+++
@@ -1,8 +0,0 @@
-document.body.onload=function(){
-
-    setTimeout(function(){
-        var preloader = document.getElementById('page_preloader');
-        if( !preloader.classList.contains('loaded'))
-        {preloader.classList.add('loaded')}
-    }, 1000);
-}
